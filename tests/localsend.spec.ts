33401325ec3168d61d174283e0a53e6f6dacfc66
Stéphane <5939522+stephane-rbn@users.noreply.github.com>
2026-01-04 16:55:17 +0100

    Add localsend spec file to end-to-end test file sharing

---
+++ tests/localsend.spec.ts
@@ -0,0 +1,47 @@
+import { expect, test } from "@playwright/test";
+import { readFileSync } from "fs";
+import path from "path";
+
+test.describe.only("LocalSend.org", () => {
+  test("should share a file", async ({ page, browser }) => {
+    await page.goto("https://web.localsend.org/");
+
+    const page2 = await browser.newPage();
+    await page2.goto("https://web.localsend.org/");
+
+    await page.bringToFront();
+
+    const recipientUserElement = page
+      .locator("div")
+      .filter({ hasText: /•/ })
+      .nth(5);
+
+    const [fileChooser] = await Promise.all([
+      page.waitForEvent("filechooser"),
+      recipientUserElement.click(),
+    ]);
+
+    await fileChooser.setFiles(path.resolve("demo.txt"));
+
+    const sendingFilesDialog = page
+      .getByRole("dialog")
+      .filter({ hasText: "Sending files..." });
+
+    await expect(sendingFilesDialog).toBeVisible();
+
+    const downloadingFilesDialog = page2
+      .getByRole("dialog")
+      .filter({ hasText: "Receiving files" });
+
+    const [download] = await Promise.all([page2.waitForEvent("download")]);
+
+    await expect(downloadingFilesDialog).toBeVisible({ timeout: 60_000 });
+
+    const downloadPath = path.resolve("downloaded-demo.txt");
+    await download.saveAs(downloadPath);
+
+    expect(readFileSync(downloadPath, { encoding: "utf-8" })).toContain(
+      "Some very important information",
+    );
+  });
+});
